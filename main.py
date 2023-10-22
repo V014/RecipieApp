33edb19cbbb23a1056029426fc658667d64a98fb
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2023-10-22 19:04:03 +0200

    added second frame

--- main.py
+++ main.py
@@ -4,6 +4,34 @@ from PIL import ImageTk
 
 bg_color ="#3d6466"
 
+def load_frame1():
+    frame1.pack_propagate(False)
+    # frame1 widgets
+    logo_img = ImageTk.PhotoImage(file="assets/img/icon.png") 
+    logo_widget = tk.Label(frame1, image=logo_img, bg=bg_color)
+    logo_widget.image = logo_img
+    logo_widget.pack()
+
+    tk.Label(frame1,
+          text="Ready for your random recipie?",
+          bg=bg_color,
+          fg="white",
+          font=("TkMenuFont", 14)
+         ).pack()
+
+    # button widget
+    tk.Button(
+        frame1,
+        text="SHUFFLE",
+        font=("TkHeadingFont", 20),
+        bg="#28393a",
+        fg="white",
+        cursor="hand2",
+        activebackground="#badee2",
+        activeforeground="black",
+        command=lambda:load_frame2()
+        ).pack(pady=20)
+
 def load_frame2():
     print("hello void")
 
@@ -19,34 +47,12 @@ root.geometry('500x500+' + str(x) + '+' + str(y))
 
 #create a frame widget
 frame1 = tk.Frame(root, width=500, height=500, bg=bg_color)
-frame1.grid(row=0, column=0)
-frame1.pack_propagate(False)
+frame2 = tk.Frame(root, bg=bg_color)
 
-# frame1 widgets
-logo_img = ImageTk.PhotoImage(file="assets/img/icon.png") 
-logo_widget = tk.Label(frame1, image=logo_img, bg=bg_color)
-logo_widget.image = logo_img
-logo_widget.pack()
+for frame in (frame1, frame2):
+    frame.grid(row=0, column=0)
 
-tk.Label(frame1,
-          text="Ready for your random recipie?",
-          bg=bg_color,
-          fg="white",
-          font=("TkMenuFont", 14)
-         ).pack()
-
-# button widget
-tk.Button(
-    frame1,
-    text="SHUFFLE",
-    font=("TkHeadingFont", 20),
-    bg="#28393a",
-    fg="white",
-    cursor="hand2",
-    activebackground="#badee2",
-    activeforeground="black",
-    command=lambda:load_frame2()
-    ).pack(pady=20)
+load_frame1()
 
 # run app
 root.mainloop()
